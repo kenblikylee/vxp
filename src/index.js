039bdfb0e0dc6f26a808c60d91604745548bac90
kenblikylee <kenblikylee@126.com>
2020-03-11 18:09:10 +0800

    export ReactAdapter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import Vxp from "./Vxp";
 import VxpPlugin from "./VxpPlugin";
 import ViewAdapter from "./ViewAdapter";
 import VueAdapter from "./VueAdapter";
+import ReactAdapter from "./ReactAdapter";
 
 export default Vxp;
 
 export {
   VxpPlugin,
   ViewAdapter,
-  VueAdapter
+  VueAdapter,
+  ReactAdapter
 }
